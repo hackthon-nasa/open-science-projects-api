3f3664d5f6828501c4d37f8111245c5c1a3f8777
gustavobruder <gugabruder@gmail.com>
2023-10-07 12:21:36 -0300

    adding json property annotation to list project endpoint response

--- OpenScienceProjects.API/Controllers/Reponses/ProjectListResponse.cs
+++ OpenScienceProjects.API/Controllers/Reponses/ProjectListResponse.cs
@@ -1,8 +1,10 @@
-﻿using OpenScienceProjects.API.Entities;
+﻿using System.Text.Json.Serialization;
+using OpenScienceProjects.API.Entities;
 
 namespace OpenScienceProjects.API.Controllers.Reponses;
 
 public class ProjectListResponse
 {
+    [JsonPropertyName("projects")]
     public IList<Project> Projects { get; set; }
 }
