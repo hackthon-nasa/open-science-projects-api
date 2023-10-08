facf408385f7f02f10244e5a2e7abfb5b377b0d9
gustavobruder <gugabruder@gmail.com>
2023-10-08 15:19:32 -0300

    adjusting entities columns max legth

---
+++ OpenScienceProjects.API/Migrations/20231008181812_AdjustedMaxLength.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenScienceProjects.API.Migrations
+{
+    public partial class AdjustedMaxLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "tag",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "project",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "title",
+                table: "project",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "organization",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "organization",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "title",
+                table: "discussion",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "discussion",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "comment",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(250)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "title",
+                table: "project");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "tag",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "project",
+                type: "varchar(250)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "organization",
+                type: "varchar(250)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "organization",
+                type: "varchar(250)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "title",
+                table: "discussion",
+                type: "varchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "discussion",
+                type: "varchar(250)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "description",
+                table: "comment",
+                type: "varchar(250)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
